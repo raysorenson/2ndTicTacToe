69d85339cf099143336a02883ad426f5734c5a36
Ray Sorenson <stngry78@gmail.com>
2022-01-24 15:07:54 -0600

    inital css commit

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 let cells = document.querySelectorAll('div :not(#board) :not(.row)') /* NodeList of 9 cells */
 
-let playerOne = 1
-if (playerOne == 1){
-    function cellClicked() {
-        event.target.textContent = 'X'
-        playerOne = 0
-} else {
-    function nextCellClicked(){
-        event.target.textContent = "O"
-        playerOne = 1
-    }
-}
-
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClicked)
         
 }
+
 
 function cellClicked() {
     event.target.textContent = 'X'
